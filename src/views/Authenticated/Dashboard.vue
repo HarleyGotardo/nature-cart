83679b5b3c65647ff9223dcba196a9805314d350
Jefone O. Gorme <jefonegorme@gmail.com>
2025-01-26 12:48:53 +0800

    feat: Add leaflet functionality

--- src/views/Authenticated/Dashboard.vue
+++ src/views/Authenticated/Dashboard.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>
-      <h2 class="text-2xl font-bold mb-4 ml-14 mt-1">Dashboard</h2>
-      <!-- Add your dashboard content here -->
-    </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  </style>
+  <div>
+    <h2 class="text-2xl font-bold mb-4 ml-14 mt-1">Dashboard</h2>
+    <!-- Map container -->
+    <div id="map" class="h-[500px] w-full"></div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default {
+  name: "Dashboard",
+  setup() {
+    onMounted(() => {
+      const map = L.map("map", {
+        dragging: false,
+        zoomControl: false,
+        scrollWheelZoom: false,
+        doubleClickZoom: false,
+        touchZoom: false,
+        boxZoom: false,
+        keyboard: false,
+      })
+      .setView([10.744340, 124.791995], 16);
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+    });
+  },
+};
+</script>
