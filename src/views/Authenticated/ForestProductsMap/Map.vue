a2ee2beb5223b771f3dc5c278bfaecaed8971723
Harley Gotardo <21-1-00404@vsu.edu.ph>
2025-01-27 12:21:49 +0800

    fix: Ensure Leaflet Icon Assets are correctly referenced and included in the build.

--- src/views/Authenticated/ForestProductsMap/Map.vue
+++ src/views/Authenticated/ForestProductsMap/Map.vue
@@ -4,6 +4,18 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { supabase } from '@/lib/supabaseClient'
 
+// Fix for the default icon issue
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
 const forestProducts = ref([])
 
 const fetchForestProducts = async () => {
